bd41cd6bcd622dca648061960c35c2bc0432e5a4
Hyeonwoo Kim <allone187@naver.com>
2023-04-04 00:00:59 +0900

    first commit

---
+++ src/main/java/kimhw/kimhwspring/KimhwSpringApplication.java
@@ -0,0 +1,13 @@
+package kimhw.kimhwspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimhwSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimhwSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kimhw/kimhwspring/KimhwSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kimhw.kimhwspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimhwSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
