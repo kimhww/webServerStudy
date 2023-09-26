dc13935d4f1d167fd43bdf664f8a4a6c76d2c371
Kim Hyeonwoo <allone187@naver.com>
2023-09-27 01:28:46 +0900

    6th_springBean_sec4

--- src/main/java/kimhw/kimhwspring/service/SpringConfig.java
+++ src/main/java/kimhw/kimhwspring/service/SpringConfig.java
@@ -13,6 +13,7 @@ public class SpringConfig {
         return new MemberService(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
